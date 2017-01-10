c294fbe13db8b4386db97df1f4df79cac5c0a875
demianvle <demianvle@protonmail.com>
2017-01-10 00:34:35 -0600

    testing

---
+++ temp_ex/src/main.rs
@@ -0,0 +1,20 @@
+  //Find the sum of all multiples of 3 or 5 below 1000.
+fn main() {
+
+  result();
+  let x = result();  
+  println!("{}", x );
+
+}
+
+
+fn result() ->i32 {
+	let mut sum = 0;
+	for i in 1..1000 {
+		if i % 3 == 0 || i % 5 == 0 {
+			sum +=i;
+
+		}	
+	}
+sum
+}
